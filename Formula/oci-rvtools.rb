eafeb63bd24474a378050d72e3256799b4357f3c
Kim Tholstorf <kim@tholstorf.dk>
2026-04-01 14:23:58 +0200

    Manual update Homebrew formula to 1.0.9

--- Formula/oci-rvtools.rb
+++ Formula/oci-rvtools.rb
@@ -1,8 +1,8 @@
 class OciRvtools < Formula
   desc "Convert RVTools Excel exports into an Oracle Cloud (OCI) monthly cost estimate workbook"
   homepage "https://github.com/KimTholstorf/oci-rvtools-cost-estimator"
-  url "https://files.pythonhosted.org/packages/source/o/oci-rvtools/oci_rvtools-1.0.7.tar.gz"
-  sha256 ""
+  url "https://files.pythonhosted.org/packages/cf/68/92a483a7a99d2d5913b04f0642c891930771b8430cba78c80ed3853808ec/oci_rvtools-1.0.9.tar.gz"
+  sha256 "b214136a38b9e67657c57f64fcef7191b1fb91287173e2193ab0b60b71b5e83d"
   license "MIT"
 
   depends_on "python3"
